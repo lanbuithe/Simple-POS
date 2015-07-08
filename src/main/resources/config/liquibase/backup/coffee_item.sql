7286b9b5a9ec6454326772224856b02ca4e4f730
The Lan Bui <lanbuithe@gmail.com>
2015-07-09 01:43:24 +0700

    Default viet nam languagle

--- src/main/resources/config/liquibase/backup/coffee_item.sql
+++ src/main/resources/config/liquibase/backup/coffee_item.sql
@@ -1,1 +1,1 @@
-INSERT INTO item VALUES (1,'Cafe Đá',12000.00,NULL,1,'admin',now(),NULL,NULL),(2,'Cafe Sữa',14000.00,NULL,1,'admin',now(),NULL,NULL),(3,'Mì Trứng',20000.00,NULL,2,'admin',now(),NULL,NULL),(4,'Mì Xào Bò',25000.00,NULL,2,'admin',now(),NULL,NULL);
+INSERT INTO item VALUES (1,'Cafe Đá',12000.00,NULL,1,'admin',now(),NULL,NULL,NULL),(2,'Cafe Sữa',14000.00,NULL,1,'admin',now(),NULL,NULL,NULL),(3,'Mì Trứng',20000.00,NULL,2,'admin',now(),NULL,NULL,NULL),(4,'Mì Xào Bò',25000.00,NULL,2,'admin',now(),NULL,NULL,NULL);
