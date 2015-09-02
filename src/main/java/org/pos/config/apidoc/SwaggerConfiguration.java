1f393ed0b1d45824672df4f148910f339cd76da2
unknown <lanbuithe@gmail.com>
2015-09-02 23:35:24 +0700

    no message

--- src/main/java/org/pos/config/apidoc/SwaggerConfiguration.java
+++ src/main/java/org/pos/config/apidoc/SwaggerConfiguration.java
@@ -21,25 +21,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import org.abcgo.config.Constants;
-import java.util.Date;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.bind.RelaxedPropertyResolver;
-import org.springframework.context.EnvironmentAware;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.core.env.Environment;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.StopWatch;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static springfox.documentation.builders.PathSelectors.regex;
-
 /**
  * Springfox Swagger configuration.
  *
