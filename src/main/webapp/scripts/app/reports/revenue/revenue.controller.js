b591a8b7e882b4a0f82ec777cd58b2a83b01f58b
The Lan Bui <lanbuithe@gmail.com>
2015-09-24 18:40:41 +0700

    Bootstrap date picker for revenue report

--- src/main/webapp/scripts/app/reports/revenue/revenue.controller.js
+++ src/main/webapp/scripts/app/reports/revenue/revenue.controller.js
@@ -1,60 +1,60 @@
 'use strict';
 
 angular.module('posApp')
-    .controller('RevenueController', ['$scope', 'moment', 'ParseLinks', 'Constants', 'Utils', 'OrderService', 
-        function ($scope, moment, ParseLinks, Constants, Utils, OrderService) {
+  .controller('RevenueController', ['$scope', 'moment', 'ParseLinks', 'Constants', 'Utils', 'OrderService', 
+    function ($scope, moment, ParseLinks, Constants, Utils, OrderService) {
 
-        var revenue = this;
+    var revenue = this;
 
-        revenue.from = moment().set('date', 1).toDate();
-        revenue.to = moment().toDate();
+    revenue.from = moment().set('date', 1).toDate();
+    revenue.to = moment().toDate();
 
-        revenue.orders = [];
-        revenue.page = 1;
-        revenue.totalAmount = 0;
+    revenue.orders = [];
+    revenue.page = 1;
+    revenue.totalAmount = 0;
 
-        revenue.downloadRevenueReport = function() {
-            var parameters = {};
-            parameters['from'] = moment(revenue.from).toISOString();
-            parameters['to'] = moment(revenue.to).toISOString();
-            Utils.downloadReport('/revenue', parameters);
-        };
+    revenue.downloadRevenueReport = function() {
+      var parameters = {};
+      parameters['from'] = moment(revenue.from).toISOString();
+      parameters['to'] = moment(revenue.to).toISOString();
+      Utils.downloadReport('/revenue', parameters);
+    };
 
-        revenue.loadAll = function() {
-            getPaymentOrder();
-            OrderService.getSumReceivableAmountByStatusCreatedDate(Constants.orderStatus.payment, revenue.from, revenue.to).then(
-                function(response) {
-                    if (!Utils.isUndefinedOrNull(response.data)) {
-                        revenue.totalAmount = response.data;
-                    }
-            });            
-        };
+    revenue.loadAll = function() {
+      getPaymentOrder();
+      OrderService.getSumReceivableAmountByStatusCreatedDate(Constants.orderStatus.payment, revenue.from, revenue.to).then(
+          function(response) {
+              if (!Utils.isUndefinedOrNull(response.data)) {
+                  revenue.totalAmount = response.data;
+              }
+      });            
+    };
 
-        revenue.search = function() {
-            revenue.page = 1;
-            revenue.orders = [];
-            revenue.loadAll();
-        };
+    revenue.search = function() {
+      revenue.page = 1;
+      revenue.orders = [];
+      revenue.loadAll();
+    };
 
-        revenue.loadPage = function(page) {
-            revenue.page = page;
-            getPaymentOrder();
-        };
+    revenue.loadPage = function(page) {
+      revenue.page = page;
+      getPaymentOrder();
+    };
 
-        function getPaymentOrder() {
-            var tableId = null;
-            OrderService.getByTableIdStatusCreatedDate(revenue.page, 6, tableId, Constants.orderStatus.payment, revenue.from, revenue.to).then(
-                function(response) {
-                    if (!Utils.isUndefinedOrNull(response.data) && 
-                        response.data.length !== 0) {
-                        revenue.links = ParseLinks.parse(response.headers('link'));
-                        for (var i = 0; i < response.data.length; i++) {
-                            revenue.orders.push(response.data[i]);
-                        }
-                    }
-            });
-        };
+    function getPaymentOrder() {
+      var tableId = null;
+      OrderService.getByTableIdStatusCreatedDate(revenue.page, 6, tableId, Constants.orderStatus.payment, revenue.from, revenue.to).then(
+          function(response) {
+              if (!Utils.isUndefinedOrNull(response.data) && 
+                  response.data.length !== 0) {
+                  revenue.links = ParseLinks.parse(response.headers('link'));
+                  for (var i = 0; i < response.data.length; i++) {
+                      revenue.orders.push(response.data[i]);
+                  }
+              }
+      });
+    };
 
-        revenue.loadAll();
+    revenue.loadAll();
 
-    }]);
+  }]);
